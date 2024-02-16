1b5794a3b03ed9aca1c7bd675f2450704f51262b
theanimated01 <79371675+theanimated01@users.noreply.github.com>
2024-02-15 18:15:44 -0800

    created CreatePage for Articles

--- frontend/src/main/pages/Articles/ArticlesCreatePage.js
+++ frontend/src/main/pages/Articles/ArticlesCreatePage.js
@@ -1,12 +1,49 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import RestaurantForm from "main/components/Articles/ArticlesForm";
+import { Navigate } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function ArticlesCreatePage() {
+export default function ArticlesCreatePage({storybook=false}) {
 
-  // Stryker disable all : articles for future implementation
+  const objectToAxiosParams = (article) => ({
+    url: "/api/articles/post",
+    method: "POST",
+    params: {
+     title: article.title,
+     url: article.url,
+     explanation: article.explanation,
+     email: article.email,
+     dateAdded: article.dateAdded
+    }
+  });
+
+  const onSuccess = (article) => {
+    toast(`New article Created - id: ${article.id} title: ${article.title}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+     { onSuccess }, 
+     // Stryker disable next-line all : hard to set up test for caching
+     ["/api/articles/all"] // mutation makes this key stale so that pages relying on it reload
+     );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/articles" />
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New Articles</h1>
+        <RestaurantForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
   )
