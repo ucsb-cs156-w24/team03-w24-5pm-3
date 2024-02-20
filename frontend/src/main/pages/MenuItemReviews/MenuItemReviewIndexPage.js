4650e1a235e77b416536b37db38fd0b299bd3ee1
Sophia <sophiattran@ucsb.edu>
2024-02-20 13:53:36 -0800

    created index page and adjusted a slight typo in the Table

--- frontend/src/main/pages/MenuItemReviews/MenuItemReviewIndexPage.js
+++ frontend/src/main/pages/MenuItemReviews/MenuItemReviewIndexPage.js
@@ -1,15 +1,45 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend';
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import MenuItemReviewsTable from 'main/components/MenuItemReviews/MenuItemReviewsTable';
+import { useCurrentUser , hasRole} from 'main/utils/currentUser'
+import { Button } from 'react-bootstrap';
 
 export default function MenuItemReviewIndexPage() {
 
-  // Stryker disable all : placeholder for future implementation
+    const currentUser = useCurrentUser();
+
+    const { data: menuItemReviews, error: _error, status: _status } =
+        useBackend(
+            // Stryker disable next-line all : don't test internal caching of React Query
+            ["/api/menuitemreviews/all"],
+            { method: "GET", url: "/api/menuitemreviews/all" },
+            // Stryker disable next-line all : don't test default value of empty list
+            []
+        );
+
+    const createButton = () => {
+        if (hasRole(currentUser, "ROLE_ADMIN")) {
+            return (
+                <Button
+                    variant="primary"
+                    href="/menuitemreviews/create"
+                    style={{ float: "right" }}
+                >
+                    Create MenuItemReview
+                </Button>
+            )
+        } 
+    }
+
   return (
     <BasicLayout>
-      <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p><a href="/menuitemreviews/create">Create</a></p>
-        <p><a href="/menuitemreviews/edit/1">Edit</a></p>
-      </div>
+        <div className="pt-2">
+            {createButton()}
+            <h1>MenuItemReviews</h1>
+            <MenuItemReviewsTable menuItemReviews={menuItemReviews} currentUser={currentUser} />
+        </div>
     </BasicLayout>
-  )
+);
 }
