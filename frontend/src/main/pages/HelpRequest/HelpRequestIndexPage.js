c86889c7a54f3740b0dcefadca70dd2fbf59d092
Chloe Ta <cqta@ucsb.edu>
2024-02-20 16:26:48 -0600

    ct - Created index page, tests, and stories. Needs debugging.

--- frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.js
+++ frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.js
@@ -1,15 +1,45 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend';
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import HelpRequestTable from 'main/components/HelpRequest/HelpRequestTable';
+import { useCurrentUser , hasRole} from 'main/utils/currentUser'
+import { Button } from 'react-bootstrap';
 
 export default function HelpRequestIndexPage() {
 
-  // Stryker disable all : placeholder for future implementation
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p><a href="/placeholder/create">Create</a></p>
-        <p><a href="/placeholder/edit/1">Edit</a></p>
-      </div>
-    </BasicLayout>
-  )
+    const currentUser = useCurrentUser();
+
+    const { data: helpRequests, error: _error, status: _status } =
+        useBackend(
+            // Stryker disable next-line all : don't test internal caching of React Query
+            ["/api/HelpRequest/all"],
+            { method: "GET", url: "/api/HelpRequest/all" },
+            // Stryker disable next-line all : don't test default value of empty list
+            []
+        );
+
+    const createButton = () => {
+        if (hasRole(currentUser, "ROLE_ADMIN")) {
+            return (
+                <Button
+                    variant="primary"
+                    href="/helprequest/create"
+                    style={{ float: "right" }}
+                >
+                    Create Help Request
+                </Button>
+            )
+        } 
+    }
+
+    return (
+        <BasicLayout>
+            <div className="pt-2">
+                {createButton()}
+                <h1>Help Requests</h1>
+                <HelpRequestTable helpRequests={helpRequests} currentUser={currentUser} />
+            </div>
+        </BasicLayout>
+    );
 }
