0fead13ba561f6257122d62cbc9355fdb31cab92
bigjr-mkkong <michaelkongboming@gmail.com>
2024-02-20 00:39:15 -0800

    added fixtures for ucsbDiningCommonsMenuItem

---
+++ frontend/src/main/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneRequest: {
+        "id": 1,
+        "diningCommonsCode": "portola",
+        "name": "chicken",
+        "station": "Main dish"
+    },
+    threeRequests: [
+        {
+            "id": 2,
+            "diningCommonsCode": "DLG",
+            "name": "cake",
+            "station": "dessert"
+        },
+        {
+            "id": 3,
+            "diningCommonsCode": "carillo",
+            "name": "beef",
+            "station": "Main dish"
+        },
+        {
+            "id": 4,
+            "diningCommonsCode": "portola",
+            "name": "curry",
+            "station": "Main dish"
+        }
+    ]
+};
+
+
+export { ucsbDiningCommonsMenuItemFixtures };
