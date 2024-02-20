57862056be81b774e61c0b2b4de8bbebae76a496
Ben Richardson <97922340+bwr02@users.noreply.github.com>
2024-02-20 03:42:36 -0800

    update placeholder links

--- frontend/src/main/pages/RecommendationRequest/RecommendationRequestIndexPage.js
+++ frontend/src/main/pages/RecommendationRequest/RecommendationRequestIndexPage.js
@@ -7,8 +7,8 @@ export default function RecommendationRequestIndexPage() {
         <BasicLayout>
             <div className="pt-2">
                 <h1>Index page not yet implemented</h1>
-                <p><a href="/placeholder/create">Create</a></p>
-                <p><a href="/placeholder/edit/1">Edit</a></p>
+                <p><a href="/recommendationrequest/create">Create</a></p>
+                <p><a href="/recommendationrequest/edit/1">Edit</a></p>
             </div>
         </BasicLayout>
     )
