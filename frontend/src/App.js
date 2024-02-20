31f502448010fc19a4d59dc358af9139a700a079
Ben Richardson <97922340+bwr02@users.noreply.github.com>
2024-02-20 00:29:33 -0800

    removed duplicate import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,10 @@ import RestaurantIndexPage from "main/pages/Restaurants/RestaurantIndexPage";
 import RestaurantCreatePage from "main/pages/Restaurants/RestaurantCreatePage";
 import RestaurantEditPage from "main/pages/Restaurants/RestaurantEditPage";
 
+import RecommendationRequestIndexPage from "main/pages/RecommendationRequest/RecommendationRequestIndexPage";
+import RecommendationRequestCreatePage from "main/pages/RecommendationRequest/RecommendationRequestCreatePage";
+import RecommendationRequestEditPage from "main/pages/RecommendationRequest/RecommendationRequestEditPage";
+
 import PlaceholderIndexPage from "main/pages/Placeholder/PlaceholderIndexPage";
 import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
 import PlaceholderEditPage from "main/pages/Placeholder/PlaceholderEditPage";
@@ -61,6 +65,21 @@ function App() {
             </>
           )
         }
+          {
+              hasRole(currentUser, "ROLE_USER") && (
+                  <>
+                      <Route exact path="/recommendationrequest" element={<RecommendationRequestIndexPage />} />
+                  </>
+              )
+          }
+          {
+              hasRole(currentUser, "ROLE_ADMIN") && (
+                  <>
+                      <Route exact path="/recommendationrequest/edit/:id" element={<RecommendationRequestEditPage />} />
+                      <Route exact path="/recommendationrequest/create" element={<RecommendationRequestCreatePage />} />
+                  </>
+              )
+          }
          {
           hasRole(currentUser, "ROLE_USER") && (
             <>
