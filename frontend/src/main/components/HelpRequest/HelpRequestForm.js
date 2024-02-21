ad3fb5951bdc502af66633257fd89df76d6e711e
Sophia <sophiattran@ucsb.edu>
2024-02-21 15:22:57 -0800

    fixed formatting in form component

--- frontend/src/main/components/HelpRequest/HelpRequestForm.js
+++ frontend/src/main/components/HelpRequest/HelpRequestForm.js
@@ -1,4 +1,4 @@
-import { Button, Form, Row, Col } from 'react-bootstrap';
+import { Button, Form, Col } from 'react-bootstrap';
 import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 
@@ -29,135 +29,128 @@ function HelpRequestForm({ initialContents, submitAction, buttonLabel = "Create"
 
         <Form onSubmit={handleSubmit(submitAction)}>
 
-
-            <Row>
-
-                {initialContents && (
-                    <Col>
-                        <Form.Group className="mb-3" >
-                            <Form.Label htmlFor="id">Id</Form.Label>
-                            <Form.Control
-                                data-testid="HelpRequestForm-id"
-                                id="id"
-                                type="text"
-                                {...register("id")}
-                                value={initialContents.id}
-                                disabled
-                            />
-                        </Form.Group>
-                    </Col>
-                )}
-
+            {initialContents && (
                 <Col>
                     <Form.Group className="mb-3" >
-                        <Form.Label htmlFor="requesterEmail">Requester Email</Form.Label>
+                        <Form.Label htmlFor="id">Id</Form.Label>
                         <Form.Control
-                            data-testid="HelpRequestForm-requesterEmail"
-                            id="requesterEmail"
+                            data-testid="HelpRequestForm-id"
+                            id="id"
                             type="text"
-                            isInvalid={Boolean(errors.requesterEmail)}
-                            {...register("requesterEmail", { required: true })}
+                            {...register("id")}
+                            value={initialContents.id}
+                            disabled
                         />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.requesterEmail && 'Requester Email is required.'}
-                        </Form.Control.Feedback>
                     </Form.Group>
                 </Col>
-                <Col>
-                    <Form.Group className="mb-3" >
-                        <Form.Label htmlFor="teamId">Team ID</Form.Label>
-                        <Form.Control
-                            data-testid="HelpRequestForm-teamId"
-                            id="teamId"
-                            type="text"
-                            isInvalid={Boolean(errors.teamId)}
-                            {...register("teamId", { required: true })}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.teamId && 'Team ID is required.'}
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                </Col>
-                <Col>
-                    <Form.Group className="mb-3" >
-                        <Form.Label htmlFor="tableOrBreakoutRoom">Table or Breakout Room</Form.Label>
-                        <Form.Control
-                            data-testid="HelpRequestForm-tableOrBreakoutRoom"
-                            id="tableOrBreakoutRoom"
-                            type="text"
-                            isInvalid={Boolean(errors.tableOrBreakoutRoom)}
-                            {...register("tableOrBreakoutRoom", { required: true })}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.tableOrBreakoutRoom && 'Table or Breakout Room is required.'}
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                </Col>
-                <Col>
-                    <Form.Group className="mb-3" >
-                        <Form.Label htmlFor="requestTime">Request Time (iso format)</Form.Label>
-                        <Form.Control
-                            data-testid="HelpRequestForm-requestTime"
-                            id="requestTime"
-                            type="datetime-local"
-                            isInvalid={Boolean(errors.requestTime)}
-                            {...register("requestTime", { required: true, pattern: isodate_regex })}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.requestTime && 'Request Time is required.'}
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                </Col>
-                <Col>
-                    <Form.Group className="mb-3" >
-                        <Form.Label htmlFor="explanation">Explanation</Form.Label>
-                        <Form.Control
-                            data-testid="HelpRequestForm-explanation"
-                            id="explanation"
-                            type="text"
-                            isInvalid={Boolean(errors.explanation)}
-                            {...register("explanation", { required: true})}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.explanation && 'Explanation is required.'}
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                </Col>
-                <Col>
-                    <Form.Group className="mb-3" >
-                        <Form.Label htmlFor="solved">Solved</Form.Label>
-                        <Form.Check
-                            data-testid="HelpRequestForm-solved"
-                            id="solved"
-                            type="switch"
-                            isInvalid={Boolean(errors.solved)}
-                            {...register("solved")}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.solved}
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                </Col>
+            )}
 
-            </Row>
+            <Col>
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="requesterEmail">Requester Email</Form.Label>
+                    <Form.Control
+                        data-testid="HelpRequestForm-requesterEmail"
+                        id="requesterEmail"
+                        type="text"
+                        isInvalid={Boolean(errors.requesterEmail)}
+                        {...register("requesterEmail", { required: true })}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.requesterEmail && 'Requester Email is required.'}
+                    </Form.Control.Feedback>
+                </Form.Group>
+            </Col>
+            <Col>
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="teamId">Team ID</Form.Label>
+                    <Form.Control
+                        data-testid="HelpRequestForm-teamId"
+                        id="teamId"
+                        type="text"
+                        isInvalid={Boolean(errors.teamId)}
+                        {...register("teamId", { required: true })}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.teamId && 'Team ID is required.'}
+                    </Form.Control.Feedback>
+                </Form.Group>
+            </Col>
+            <Col>
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="tableOrBreakoutRoom">Table or Breakout Room</Form.Label>
+                    <Form.Control
+                        data-testid="HelpRequestForm-tableOrBreakoutRoom"
+                        id="tableOrBreakoutRoom"
+                        type="text"
+                        isInvalid={Boolean(errors.tableOrBreakoutRoom)}
+                        {...register("tableOrBreakoutRoom", { required: true })}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.tableOrBreakoutRoom && 'Table or Breakout Room is required.'}
+                    </Form.Control.Feedback>
+                </Form.Group>
+            </Col>
+            <Col>
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="requestTime">Request Time (iso format)</Form.Label>
+                    <Form.Control
+                        data-testid="HelpRequestForm-requestTime"
+                        id="requestTime"
+                        type="datetime-local"
+                        isInvalid={Boolean(errors.requestTime)}
+                        {...register("requestTime", { required: true, pattern: isodate_regex })}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.requestTime && 'Request Time is required.'}
+                    </Form.Control.Feedback>
+                </Form.Group>
+            </Col>
+            <Col>
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="explanation">Explanation</Form.Label>
+                    <Form.Control
+                        data-testid="HelpRequestForm-explanation"
+                        id="explanation"
+                        type="text"
+                        isInvalid={Boolean(errors.explanation)}
+                        {...register("explanation", { required: true})}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.explanation && 'Explanation is required.'}
+                    </Form.Control.Feedback>
+                </Form.Group>
+            </Col>
+            <Col>
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="solved">Solved</Form.Label>
+                    <Form.Check
+                        data-testid="HelpRequestForm-solved"
+                        id="solved"
+                        type="switch"
+                        isInvalid={Boolean(errors.solved)}
+                        {...register("solved")}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.solved}
+                    </Form.Control.Feedback>
+                </Form.Group>
+            </Col>
 
-            <Row>
-                <Col>
-                    <Button
-                        type="submit"
-                        data-testid="HelpRequestForm-submit"
-                    >
-                        {buttonLabel}
-                    </Button>
-                    <Button
-                        variant="Secondary"
-                        onClick={() => navigate(-1)}
-                        data-testid="HelpRequestForm-cancel"
-                    >
-                        Cancel
-                    </Button>
-                </Col>
-            </Row>
+            <Col>
+                <Button
+                    type="submit"
+                    data-testid="HelpRequestForm-submit"
+                >
+                    {buttonLabel}
+                </Button>
+                <Button
+                    variant="Secondary"
+                    onClick={() => navigate(-1)}
+                    data-testid="HelpRequestForm-cancel"
+                >
+                    Cancel
+                </Button>
+            </Col>
         </Form>
 
     )
